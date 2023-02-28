3d44c81acfdc4bca175d87e71f76f9523ce5ed6c
INSTALL <INSTALL@L2018Q3CLG.corp.telma.mg>
2023-02-28 10:41:18 +0300

    DES RDV TS

---
+++ Test Suites/TELMA_DES_RDV/DES_RDV.ts
@@ -0,0 +1,28 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>DES_RDV</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>ce4e184a-72ff-4fe6-be76-c24912aaaa8e</testSuiteGuid>
+   <testCaseLink>
+      <guid>83d3b123-6479-4f0e-b732-f1d77560c048</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DES_RDV_TEM/prendre_rdv</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+   <testCaseLink>
+      <guid>8fe2c7d1-0bcf-4f20-a0f5-6ca59af70cf9</guid>
+      <isReuseDriver>false</isReuseDriver>
+      <isRun>true</isRun>
+      <testCaseId>Test Cases/DES_RDV_TEM/delete_rdv</testCaseId>
+      <usingDataBindingAtTestSuiteLevel>true</usingDataBindingAtTestSuiteLevel>
+   </testCaseLink>
+</TestSuiteEntity>
